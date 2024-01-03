201a3fcc574291e8b8e24c1a2f374c4f02ba8e9a
Bae seoyoung <86220074+bsyseo@users.noreply.github.com>
2024-01-03 09:28:05 +0900

    Create dash3.vue

---
+++ dash3.vue
@@ -0,0 +1,35 @@
+<template>
+    <div> 
+        <div class="title-border">감축목표</div>
+        <carbonLastBarGraph :key="re"/>
+        <targetList :key="re"/>
+    </div>
+        
+
+</template>
+
+
+<script>
+import carbonLastBarGraph from './carbonLastBarGraph.vue';
+import targetList from './targetList.vue';
+import {useStore} from 'vuex'
+import {computed} from 'vue'
+    export default {
+        name :"target_dash3",
+        components:{
+            carbonLastBarGraph,
+            targetList
+        
+        },
+        setup(){    
+            var store = useStore()
+            var re = computed(()=>store.state.reload)
+            return{re}
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
